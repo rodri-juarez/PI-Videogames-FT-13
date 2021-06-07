2f5b53e45a6b1a44bfa783621e2e694835f98eea
rodrigo <juarezr@gmail.com>
2021-06-07 15:44:57 -0300

    db changes

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,9 +1,41 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
+
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define("videogame", {
+   /*  id: {
+      type: DataTypes.INTEGER primaryKey,
+      allowNull: false,
+    }, */
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+
+    plataform: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    released: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+
+    rating: {
+      type: DataTypes.NUMERIC,
+    },
+  });
+
+  sequelize.define("genres", {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
