2c70ccfef1bd4ba05fd32ac5ea9379bc5c391f3f
rodrigo juarez <juarezr100@gmail.com>
2021-08-03 11:37:41 -0300

    feat(api): change background image

--- client/src/components/paginaInicial/PaginaInicial.jsx
+++ client/src/components/paginaInicial/PaginaInicial.jsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
 import style from "./paginaInicial.module.css";
 export default function PaginaInicial() {
-  return (
+  return (<>
+  <div className={style.divH1}>
+    <h1 className={style.h1}>Welcome to Henry Videogames!</h1>
+    </div>
+      <Link className={style.link} to="/Home">
+        <button className={style.btn}> HOME</button>
+      </Link>
     <div className={style.div}>
       <div className={style.button}>
-        <h1 className={style.h1}>Welcome to Henry Videogames!</h1>
 
-        
+
       </div>
-      <Link className={style.link} to="/Home">
-          <button className={style.btn}> HOME</button>
-        </Link>
     </div>
+  </>
   );
 }
