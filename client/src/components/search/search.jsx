529af2cfc7cd591dfa2979757708e239df81f692
rodrigo juarez <juarezr100@gmail.com>
2021-08-04 14:39:25 -0300

    refactor(client/search): add @media min/max 500/850px

--- client/src/components/search/search.jsx
+++ client/src/components/search/search.jsx
@@ -9,22 +9,16 @@ import { Link } from 'react-router-dom';
 import style from "./search.module.css";
 
 const useStyles = makeStyles((theme) => ({
-    
     input: {
         marginLeft: theme.spacing(1),
         flex: 1,
         color: 'rgb(255, 255, 255)',
-        placeholder: 'center'
+        
     },
     iconButton: {
         padding: 10,
         color: 'rgb(255, 255, 255)',
     },
-    divider: {
-        height: 28,
-        margin: 4,
-    },
-
 }));
 
 
@@ -45,7 +39,7 @@ export default function Search({ busquedaPorNombre }) {
             <IconButton className={classes.iconButton} aria-label="menu">
                 <Link className={style.Link} to="/Home">Home</Link>
             </IconButton>
-            <Divider className={classes.divider} orientation="vertical" />
+            <Divider className={style.divider} orientation="vertical" />
             <InputBase
                 className={classes.input}
                 placeholder="Search videogame"
@@ -55,7 +49,7 @@ export default function Search({ busquedaPorNombre }) {
             <IconButton type="submit" className={classes.iconButton} aria-label="search">
                 <SearchIcon />
             </IconButton>
-            <Divider className={classes.divider} orientation="vertical" />
+            <Divider className={style.divider} orientation="vertical" />
             <IconButton color="primary" className={classes.iconButton} aria-label="directions">
                 <Link className={style.Link} to="/CreateVideogame">Create Videogame</Link>
             </IconButton>
