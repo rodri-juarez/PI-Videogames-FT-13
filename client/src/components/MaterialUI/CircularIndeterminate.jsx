4f09d4fcdd247e91398be48944752aa312944b65
rodrigo juarez <juarezr100@gmail.com>
2021-08-04 10:32:41 -0300

    refactor(client): change size of spinner

--- client/src/components/MaterialUI/CircularIndeterminate.jsx
+++ client/src/components/MaterialUI/CircularIndeterminate.jsx
@@ -4,9 +4,9 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundColor: 'rgb(42, 42, 42);',
-    height:'30vw',
-    width: '30vw',
+    backgroundColor: 'rgb(42, 42, 42)',
+    height:'45vw',
+    width: '100vw',
     display: 'flex',
     '& > * + *': {
       marginLeft: theme.spacing(2),
@@ -14,7 +14,7 @@ const useStyles = makeStyles((theme) => ({
     justifyContent:'center',
     alignContent:'center',
     alignItems:'center',
-       
+    
   },
 }));
 
