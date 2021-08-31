e5ec2609baddc10f2ff1c3d4ca1fa5ceea82067d
rodrigo juarez <juarezr100@gmail.com>
2021-08-31 09:48:25 -0300

    refactor(app): move to folder in containers

--- client/src/containers/app/App.js
+++ client/src/containers/app/App.js
@@ -1,18 +1,18 @@
 import React, { Suspense, lazy } from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import CircularIndeterminate  from '../components/MaterialUI/CircularIndeterminate';
+import CircularIndeterminate  from '../../components/MaterialUI/CircularIndeterminate';
 import "./App.css";
 
 const PaginaInicial = lazy(() =>
-  import("../components/paginaInicial/PaginaInicial")
+  import("../../components/paginaInicial/PaginaInicial")
 );
-const Home = lazy(() => import("./home/index"));
-const CreateVideogame = lazy(() => import("../components/createVideogame"));
+const Home = lazy(() => import("../home/index"));
+const CreateVideogame = lazy(() => import("../../components/createVideogame"));
 
 const VideogameDetail = lazy(() =>
-  import("../components/videogameDetail/index.jsx")
+  import("../../components/videogameDetail/index.jsx")
 );
-const NotFound = lazy(() => import("../components/notFound"));
+const NotFound = lazy(() => import("../../components/notFound"));
 
 function App() {
   return (
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App.js';
+import App from './containers/app/App.js';
 import reportWebVitals from './reportWebVitals';
 import store from "./store/index";
 import { Provider } from "react-redux";
